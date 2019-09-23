578c9e451cc40cbbe686c4b77df1960d5aea2fb2
ThomasTW935 <49426473+ThomasTW935@users.noreply.github.com>
2019-09-23 16:57:56 +0800

    Update UserInput.java

--- src/review/UserInput.java
+++ src/review/UserInput.java
@@ -6,10 +6,30 @@ public class UserInput {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		System.out.println("Type a number");
-		int newNum = s.nextInt();
-		System.out.println(newNum);
-
+		
+		//For String/Character
+		System.out.println("Type a text");
+		String newString = s.nextLine(); // or s.next();
+		System.out.println("Output: " + newString);
+		
+		//For Integer
+		System.out.println("Type a Integer");
+		int newInt = s.nextInt();
+		System.out.println("Output: " + newInt);
+		
+		//For Double
+		System.out.println("Type a Double");
+		double newDouble = s.nextDouble();
+		System.out.println("Output: " + newDouble);
+		
+		//For Float
+		System.out.println("Type a Float");
+		float newFloat= s.nextFloat();
+		System.out.println("Output: " + newFloat);
+		
+				
+		System.out.println();
+		
 	}
 
 }
