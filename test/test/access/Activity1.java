0fe9d264348f3673aa37045216615fd1f417b598
d0ntbotherme <chris_tine09@yahoo.com>
2019-10-10 00:36:34 +0800

    magandaako
    .magandaako

--- test/test/access/Activity1.java
+++ test/test/access/Activity1.java
@@ -1,24 +1,20 @@
 package test.access;
 
+import review.access.sub.Daryl;
+
 /*
- * 1. Create another class, name it "Activity1Sub"
- * 2. create a String name = <Your Name>  in the newly created class
- *    then output it in this class
- *  
- * 3. create a function that returns a double  in "Activity1Sub"
- * 4. create two double datatype value of the first double is 105.5
- *    and the second double is 15
- *    substract the first and second  then store it to total
- *    return total 
- * 5. and print the output in this class 
- *  
+  1. Create another class, name it "Activity1Sub"
+  2. create a String name = <Your Name>  in the newly created class
+     then output it in this class
+   
+  3. create a function that returns a double  in "Activity1Sub"
+  4. create two double datatype value of the first double is 105.5
+     and the second double is 15
+     substract the first and second  then store it to total
+     return total 
+  5. and print the output in this class 
+  
  */
 
-public class Activity1 {
 
-	public static void main(String[] args) {
-		
 
-	}
-
-}
---
+++ test/test/access/Activity1Sub.java
@@ -0,0 +1,18 @@
+package test.access;
+
+public class Activity1Sub 
+{
+	public String chris = "Christine Aparato";
+
+
+	public double divide(double num1, double num2)
+	{
+		double total = num1/num2;
+		return total;
+	}	
+	public String info(String name, int age, char gender)
+	{
+		String info = (name + " "+age+ " "+ gender);
+		return info;
+	}
+}	dferkhtowehoiufhowefhiowqeh
