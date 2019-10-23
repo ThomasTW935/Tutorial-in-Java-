f596834895ebe6cff808b7e508b19f059424e328
ThomasTW935 <49426473+ThomasTW935@users.noreply.github.com>
2019-10-23 15:58:31 +0800

    Oct 23

---
+++ DuplicateOutput/daryl/StudentInfo.java
@@ -0,0 +1,26 @@
+package daryl;
+
+import java.util.Scanner;
+
+public class StudentInfo {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		StudentInfoSub st = new StudentInfoSub();
+		System.out.println("What is your First Name?");
+		st.setFirstName(s.nextLine());
+		System.out.println("What is your Middle Initial?");
+		st.setMiddleInitial(s.nextLine());
+		System.out.println("What is your Last Name?");
+		st.setLastName(s.nextLine());
+		System.out.println("What is your Gender? M or F");
+		st.setGender(s.nextLine());
+		System.out.println("How old are you?");
+		st.setAge(s.nextInt());
+		System.out.println("What is your Student ID?");
+		st.setStudentID(s.nextLong());
+		
+		st.printInfo();
+	}
+
+}
---
+++ DuplicateOutput/daryl/StudentInfoSub.java
@@ -0,0 +1,54 @@
+package daryl;
+
+public class StudentInfoSub {
+	String firstName;
+	String middleInitial;
+	String lastName;
+	String gender;
+	int age;
+	long studentID;
+	public void printInfo() {
+		String fullName = getLastName()+ ", " + getFirstName() + " " + getMiddleInitial();
+		System.out.println("Name: "+fullName);
+		System.out.println("Gender: "+ gender);
+		System.out.println("Age: "+ age);
+		System.out.println("Student ID: "+ getStudentID());
+	}
+	
+	private String getFirstName() {
+		return firstName;
+	}
+	private String getMiddleInitial() {
+		return middleInitial;
+	}
+	private String getLastName() {
+		return lastName;
+	}
+	private String getGender() {
+		return gender;
+	}
+	private int getAge() {
+		return age;
+	}
+	private long getStudentID() {
+		return studentID;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public void setMiddleInitial(String middleInitial) {
+		this.middleInitial = middleInitial;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public void setStudentID(long studentID) {
+		this.studentID = studentID;
+	}
+}
