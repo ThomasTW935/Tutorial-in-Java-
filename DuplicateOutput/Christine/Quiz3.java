f5af5adc5f1e65a4b4d56bc367201d21f6e9b8e8
ThomasTW935 <49426473+ThomasTW935@users.noreply.github.com>
2019-11-18 22:07:40 +0800

    Quiz 3 & 4

---
+++ DuplicateOutput/Christine/Quiz3.java
@@ -0,0 +1,28 @@
+package Christine;
+
+public class Quiz3 {
+
+	public static void main(String[] args) {
+			String [] FirstName = {"Christine", "Hush","Daryl"};
+			String [] LastName = {"Aparato", "De peralta", "Thomas"};
+			
+			
+			
+			for (int i=0; i<FirstName.length;i++)
+			{
+				int Age = 20;
+				if(FirstName[i].equals("Christine")) {
+					Age = 31;
+				}
+				String Gender = "Male";
+				if(FirstName[i].equals("Christine") &&LastName[i].equals("Aparato")) {
+					Gender = "Female";
+				
+				}
+				System.out.println(FirstName[i]+ " "+LastName[i]);
+				System.out.println(Age);
+				System.out.println(Gender);
+			}
+	}
+
+}
---
+++ DuplicateOutput/Christine/Quiz4.java
@@ -0,0 +1,20 @@
+package Christine;
+
+public class Quiz4
+{
+ public static void main (String[]args)
+{
+	 
+	 Quiz4 tin = new Quiz4();
+	 String name = "Christ";
+	 tin.print(name);
+	 
+
+}
+ public void print(String name)
+ {
+	 //String name = "Christine Aparato";
+	 
+ System.out.print("Name:"+ name);
+ }
+ }
